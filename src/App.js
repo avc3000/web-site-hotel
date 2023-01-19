5b562424bf2cdb97505d0da3f92d47c7565e9e05
Antony Jordan Villar Cristobal <antony.villar.rp@viaexperis.pe>
2023-01-19 10:28:47 -0500

    first Commit.

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import RoomDetails from './pages/RoomDetails';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/room/:id',
+    element: <RoomDetails />
+  }
+]);
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/HeroSlider.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import { EffectFade, Autoplay } from 'swiper';
+import Img1 from '../assets/img/heroSlider/1.jpg';
+import Img2 from '../assets/img/heroSlider/2.jpg';
+import Img3 from '../assets/img/heroSlider/3.jpg';
+
+const slides = [
+  {
+    title: 'Your Luxury Hotel For Vacation',
+    bg: Img1,
+    btnText: 'Room our Rooms'
+  },
+  {
+    title: 'Your Luxury Hotel For Vacation',
+    bg: Img2,
+    btnText: 'Room our Rooms'
+  },
+  {
+    title: 'Your Luxury Hotel For Vacation',
+    bg: Img3,
+    btnText: 'Room our Rooms'
+  },
+]
+
+const HeroSlider = () => {
+  return (
+    <Swiper modules={[EffectFade, Autoplay]} effect={'fade'} loop={true} autoplay={{delay: 2000, disableOnInteraction: false}} className='heroSlider h-[600px] lg:h-[860px]'>
+      {
+        slides.map((slide, index) => {
+
+          const { title, bg, btnText } = slide;
+
+          return (
+            <SwiperSlide key={index} className='h-full bg-pink-400 relative flex justify-center items-center'>
+              <div className='z-20 text-white text-center'>
+                <div className='uppercase font-tertiary tracking-[6px] mb-5'>Just Enjoy and Relax</div>
+                <h1 className='text-[32px] font-primary uppercase tracking-[2px] max-w-[920px] lg:text-[68px] leading-tight mb-6'>{title}</h1>
+                <button className='btn btn-lg btn-primary mx-auto'>{btnText}</button>
+              </div>
+              <div className='absolute top-0 w-full h-full'>
+                <img src={bg} alt='' className='object-cover h-full w-full' />
+              </div>
+              <div className='absolute w-full h-full bg-black/70'></div>
+            </SwiperSlide>
+          )
+        })
+      }
+    </Swiper>
+  );
+};
+
+export default HeroSlider;
